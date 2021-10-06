3d377833e2689c550e66660ea84fa30c1c1d7665
Carlos Vera <kikiondo117@gmail.com>
2021-10-06 12:33:37 -0500

    Share facebook test

--- src/app/App..jsx
+++ src/app/App..jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { AppStyles } from "./App.styles";
 // * Assets
 import logo from "img/RADIO-1-TINTA-VERTICAL-BLANCO.svg";
@@ -5,6 +6,14 @@ import logo from "img/RADIO-1-TINTA-VERTICAL-BLANCO.svg";
 function App({ children }) {
   return (
     <AppStyles>
+      <Helmet>
+        <meta property="og:title" content="Radio chilanga" />
+        <meta
+          property="og:description"
+          content="Musica Méxicana, clasicas y nuevas."
+        />
+        <meta property="og:image" content={logo} />
+      </Helmet>
       <img
         src={logo}
         style={{ width: "80px", position: "fixed", top: 10 }}
